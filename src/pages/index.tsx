7ddeb7d4952685a79e73ae00e0b3a224ff244e66
ruanjingtao <479118658@qq.com>
2024-06-25 18:06:43 +0800

    feat: 列表渲染和动态更新数据

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,32 @@
-import yayJpg from '../assets/yay.jpg';
+import { useState } from "react";
+import yayJpg from "../assets/yay.jpg";
 
 export default function HomePage() {
+  const [data, setData] = useState([
+    {
+      name: "yay",
+      id: 1,
+    },
+    {
+      name: "rjt",
+      id: 2,
+    },
+    {
+      name: "aba",
+      id: 3,
+    },
+  ]);
+  function addItem() {
+    setData([...data, { name: "yay", id: 4 }]);
+  }
   return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-    </div>
+    <>
+      <ul>
+        {data.map((item, index) => (
+          <li key={index}>{item.name}</li>
+        ))}
+      </ul>
+      <button onClick={addItem}>点我加一条</button>
+    </>
   );
 }
