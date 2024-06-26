93e74982d18e21e57af888f4e9ae5ee014d39665
ruanjingtao <479118658@qq.com>
2024-06-26 14:46:04 +0800

    feat: 持久化实现

--- src/pages/todo.tsx
+++ src/pages/todo.tsx
@@ -10,19 +10,6 @@ const TodoListPage = () => {
   // 定义filter状态
   const [filter, setFilter] = useState("all");
 
-  // useEffect(() => {
-  //   // 获取todos
-  //   const todosFromStorage = localStorage.getItem("todos");
-  //   if (todosFromStorage) {
-  //     setTodos(JSON.parse(todosFromStorage));
-  //     console.log("挂载todo", todos);
-  //   }
-  //   return () => {
-  //     console.log("卸载todo", todos);
-  //     localStorage.setItem("todos", JSON.stringify(todos));
-  //   };
-  // }, []);
-
   useEffect(() => {
     // 获取todos
     const todosFromStorage = localStorage.getItem("todos");
@@ -31,14 +18,11 @@ const TodoListPage = () => {
       console.log("挂载todo", todos);
     }
   }, []);
-  console.log(todos, "todos");
 
   useEffect(() => {
-    return () => {
-      console.log("卸载todo", todos);
-      localStorage.setItem("todos", JSON.stringify(todos));
-    };
-  }, []);
+    console.log("todo变化", todos);
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   // 定义添加todo的方法
   const addTodo = (text: string) => {
@@ -49,7 +33,7 @@ const TodoListPage = () => {
       completed: false,
     };
     setTodos([...todos, newTodo]);
-    console.log("添加todo", newTodo, todos);
+    // console.log("添加todo", newTodo, todos);
   };
 
   // 定义删除todo的方法
