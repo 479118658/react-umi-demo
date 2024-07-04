9aa03826fc113f36cf7b449a36ea8b07e4508e4a
ruanjingtao <479118658@qq.com>
2024-07-04 15:20:39 +0800

    feat: ahooks引入

--- config/route.ts
+++ config/route.ts
@@ -1,9 +1,19 @@
+import { Children } from "react";
+
 export default [
   { path: "/", component: "index", name: "首页" },
-  { path: "/docs", component: "docs/index", name: "组件通信" },
-  { path: "/hooks", component: "hooks/index", name: "hooks" },
+  {
+    path: "/docs",
+    name: "组件通信",
+    routes: [
+      { path: "/docs/provider", component: "docs/provider", name: "provider" },
+      { path: "/docs/index", component: "docs/index", name: "index" },
+    ],
+  },
+  { path: "/hooks", component: "hooks/index", name: "自定义hooks" },
   { path: "/antd", component: "antd/index", name: "antd" },
   { path: "/zustand", component: "zustand/index", name: "状态管理zustand" },
+  { path: "/ahooks", component: "ahooks/index", name: "ahooks" },
   { path: "/todo", component: "todo", name: "todo" },
   { path: "/*", component: "@/pages/404.tsx" },
 ];
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -4,11 +4,28 @@ import { Link, Outlet, useAppData, useLocation } from "umi";
 export default function Layout() {
   const { clientRoutes } = useAppData();
   const location = useLocation();
+  const routes = [
+    {
+      path: "/task-center",
+      breadcrumbName: "任务中心",
+    },
+    {
+      path: "/task-details",
+      breadcrumbName: "任务详情",
+    },
+    {
+      path: "/current",
+      breadcrumbName: "当前页面",
+    },
+  ];
   return (
     <ProLayout
       route={clientRoutes[0]}
       location={location}
       title="阮景涛第一个umi项目"
+      breadcrumbRender={() => {
+        return routes;
+      }}
       menuItemRender={(menuItemProps, defaultDom) => {
         if (menuItemProps.isUrl || menuItemProps.children) {
           return defaultDom;
