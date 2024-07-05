6a44f6bfa3384040526dc87f85ac2e3129b78566
ruanjingtao <479118658@qq.com>
2024-07-05 16:23:11 +0800

    feat: 限制promise返回类型，以及zustand异步请求userequset

--- src/pages/zustand/index.tsx
+++ src/pages/zustand/index.tsx
@@ -2,16 +2,26 @@ import { Button } from "antd"; // 导入antd库中的Button组件
 import "./index.less";
 import { useStore } from "./store";
 import Hello from "./hello";
+import { useRequest } from "ahooks";
 // 定义一个ZuStand函数，用于渲染返回一个div标签
 function ZuStand() {
   const { count, increment, decrement, fetchData, list } = useStore(); // 从状态管理器中获取状态
+  const { loading, data, run } = useRequest(fetchData, {
+    manual: true,
+    onSuccess: (res) => {
+      console.log("ahooks接口返回数据", data);
+    },
+  });
+
   return (
     <>
       <div>ZuStand, count: {count}</div>
       <Button onClick={increment}>+1</Button>
       <Button onClick={decrement}>-1</Button>
       <Button onClick={() => useStore.getState().reset()}>重置</Button>
-      <Button onClick={fetchData}>接口</Button>
+      <Button onClick={run} disabled={loading}>
+        接口
+      </Button>
 
       <Hello />
     </>
--- src/pages/zustand/store.ts
+++ src/pages/zustand/store.ts
@@ -1,8 +1,24 @@
 import { create } from "zustand"; // 导入zustand库中的create函数
 import axios from "axios";
 
+// 定义一个getData函数，用于模拟异步请求
+function getData(path: string): Promise<[]> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      axios
+        .get(path)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    }, 1000);
+  });
+}
+
+// 使用create函数创建一个状态管理器
 export const useStore = create((set: any) => ({
-  // 使用create函数创建一个状态管理器
   // 初始化一个count变量，值为0
   count: 0,
   list: [],
@@ -12,10 +28,15 @@ export const useStore = create((set: any) => ({
   decrement: () => set((state: any) => ({ count: state.count - 1 })),
   // 定义一个reset方法，用于重置count的值为0
   reset: () => set({ count: 0, list: [] }),
+  // 定义一个fetchData方法，用于模拟异步请求
   fetchData: async () => {
-    // 模拟异步请求
-    const { data } = await axios.get("/api/todos");
-    console.log("data", data);
-    set({ list: data });
+    try {
+      const data = await getData("/api/todos");
+      console.log("data", data);
+      set({ list: data });
+      return Promise.resolve(data);
+    } catch (error) {
+      console.log(error);
+    }
   },
 }));
